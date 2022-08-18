eae7f95414227129e20ff45e9b815bc4b36bc184
anlen.wang <anlen.wang@idreamsky.com>
2022-08-18 10:11:28 +0800

    add

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function treeNode(data, pid) {
     }
     return arr;
 }
+
+// 数组扁平化
+function flatten(arr) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            result = result.concat(flatten(arr[i]));
+        } else {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
