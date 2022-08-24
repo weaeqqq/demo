d469ff4059c33e1fbdc7e20be9e745f64b75b1cb
anlen.wang <anlen.wang@idreamsky.com>
2022-08-24 11:15:09 +0800

    http浏览器通知

--- index.js
+++ index.js
@@ -1,111 +1,152 @@
 // 递归树节点
 function treeNode(data, pid) {
-    var arr = [];
-    for (var i = 0; i < data.length; i++) {
-        var node = data[i];
-        if (node.pid == pid) {
-            arr.push(node);
-            arr = arr.concat(treeNode(data, node.id));
-        }
+  var arr = [];
+  for (var i = 0; i < data.length; i++) {
+    var node = data[i];
+    if (node.pid == pid) {
+      arr.push(node);
+      arr = arr.concat(treeNode(data, node.id));
     }
-    return arr;
+  }
+  return arr;
 }
 
 // 数组扁平化
 function flatten(arr) {
-    var result = [];
-    for (var i = 0; i < arr.length; i++) {
-        if (Array.isArray(arr[i])) {
-            result = result.concat(flatten(arr[i]));
-        } else {
-            result.push(arr[i]);
-        }
+  var result = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(flatten(arr[i]));
+    } else {
+      result.push(arr[i]);
     }
-    return result;
+  }
+  return result;
 }
 
 // 数组去重
 function unique(arr) {
-    var result = [];
-    for (var i = 0; i < arr.length; i++) {
-        if (result.indexOf(arr[i]) == -1) {
-            result.push(arr[i]);
-        }
+  var result = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (result.indexOf(arr[i]) == -1) {
+      result.push(arr[i]);
     }
-    return result;
+  }
+  return result;
 }
 
 // 数组排序
 function sort(arr) {
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = 0; j < arr.length; j++) {
-            if (arr[i] < arr[j]) {
-                var temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
     }
-    return arr;
+  }
+  return arr;
 }
 
 // 数组最大
-const max = arr => Math.max.apply(null, arr);
+const max = (arr) => Math.max.apply(null, arr);
 
 // 数组切割
 const chunk = (arr, size) => {
-    var result = [];
-    for (var i = 0; i < arr.length; i += size) {
-        result.push(arr.slice(i, i + size));
-    }
-    return result;
-}
+  var result = [];
+  for (var i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
+};
 
 // 匹配非0的整数
-const matchInt = str => {
-    var reg = /^[1-9]\d*$/;
-    return reg.test(str);
-}
+const matchInt = (str) => {
+  var reg = /^[1-9]\d*$/;
+  return reg.test(str);
+};
 
 // 匹配价格
-const matchPrice = str => {
-    var reg = /^[0-9]+(.[0-9]{1,2})?$/;
-    return reg.test(str);
-}
+const matchPrice = (str) => {
+  var reg = /^[0-9]+(.[0-9]{1,2})?$/;
+  return reg.test(str);
+};
 
 // 匹配1-100的整数
-const match100 = str => {
-    var reg = /^[1-9]\d?$/;
-    return reg.test(str);
-}
+const match100 = (str) => {
+  var reg = /^[1-9]\d?$/;
+  return reg.test(str);
+};
 
 // 浮点数计算精度
 const accMul = (options) => {
-    const {num: arg1, num2: arg2, type = "addition", decimal = 2 } = options;
-    const types = {
-        addition: 'addition', // 加法
-        subtraction: 'subtraction', // 减法
-        multiplication: 'multiplication', // 乘法
-        division: 'division' // 除法
-    }
+  const { num1: arg1, num2: arg2, type = "addition", decimal = 2 } = options;
+  const types = {
+    addition: "addition", // 加法
+    subtraction: "subtraction", // 减法
+    multiplication: "multiplication", // 乘法
+    division: "division", // 除法
+  };
 
-    const s1 = arg1.toString(), s2 = arg2.toString();
-    const m1 = s1.split('.')[1] ? s1.split('.')[1].length : 0;
-    const m2 = s2.split('.')[1] ? s2.split('.')[1].length : 0;
-    const max = Math.max(m1, m2);
-    const m = Math.pow(10, max);
-    let result = 0;
-    
-    if (type == types.addition) {
-        result = (arg1 * m + arg2 * m) / m;
-    } else if (type == types.subtraction) {
-        result = (arg1 * m - arg2 * m) / m;
-    } else if (type == types.multiplication) {
-        result = (arg1 * m * arg2 * m) / (m * m);
-    } else if (type == types.division) {
-        result = (arg1 * m / arg2 * m) / (m * m);
-    }
+  const s1 = arg1.toString(),
+    s2 = arg2.toString();
+  const m1 = s1.split(".")[1] ? s1.split(".")[1].length : 0;
+  const m2 = s2.split(".")[1] ? s2.split(".")[1].length : 0;
+  const max = Math.max(m1, m2);
+  const m = Math.pow(10, max);
+  let result = 0;
 
-    result = result.toString().split('.')[1]?.length > 2 ? result.toFixed(decimal) : result;
-    return result;
-}
+  if (type == types.addition) {
+    result = (arg1 * m + arg2 * m) / m;
+  } else if (type == types.subtraction) {
+    result = (arg1 * m - arg2 * m) / m;
+  } else if (type == types.multiplication) {
+    result = (arg1 * m * arg2 * m) / (m * m);
+  } else if (type == types.division) {
+    result = (((arg1 * m) / arg2) * m) / (m * m);
+  }
+
+  result =
+    result.toString().split(".")[1]?.length > 2
+      ? result.toFixed(decimal)
+      : result;
+  return parseFloat(result);
+};
+
+// 生成全局唯一ID
+// 国外stackoverflow大佬： https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
+// 链接对比：https://cythilya.github.io/2017/03/12/uuid/
+const getUUID = () => {
+  var d = Date.now();
+  if (
+    typeof performance !== "undefined" &&
+    typeof performance.now === "function"
+  ) {
+    d += performance.now();
+  }
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+};
+
+// chrome桌面提醒
+const notify = async (options) => {
+  // 先获取通知接口权限
+  var status = await Notification.requestPermission() === "granted";
+  if (!status) {
+    return;
+  }
+
+  var popNotice = function () {
+    if (Notification.permission == "granted") {
+      var notification = new Notification("Hi，帅哥：", {
+        body: "视频需要验证，请点击按钮进行验证",
+        icon: "//image.zhangxinxu.com/image/study/s/s128/mm1.jpg",
+      });
+    }
+  };
+  popNotice();
+};
