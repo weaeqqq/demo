72348eba58dee11cb1477acdf03066a0e3218de9
anlen.wang <anlen.wang@idreamsky.com>
2022-08-29 14:51:15 +0800

    empty

--- index.js
+++ index.js
@@ -151,3 +151,16 @@ const notify = async (options) => {
   };
   popNotice();
 };
+
+
+// 判断是否为空
+const isEmpty = (obj) => {
+  if (obj == null) return true;
+  if (obj.length > 0) return false;
+  if (obj.length === 0) return true;
+  if (typeof obj !== "object") return true;
+  for (var key in obj) {
+    if (hasOwnProperty.call(obj, key)) return false;
+  }
+  return true;
+}
